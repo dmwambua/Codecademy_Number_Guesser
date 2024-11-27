07fc5c89c634f0852bcb51213ca86fa855b69920
dmwambua <dickmuoki@gmail.com>
2024-11-26 21:10:30 -0500

    finished coding Challenge Project: Number Guesser

--- script.js
+++ script.js
@@ -5,5 +5,29 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget =() =>{
-return Math.floor(Math.random())*9;
+return Math.floor(Math.random()*10);
+}
+const compareGuesses = (humanGuess, computerGuess, secretNum) =>{
+let humanGuessDifference = Math.abs(secretNum-humanGuess);
+let compGuessDifference = Math.abs(secretNum-computerGuess);
+if (humanGuessDifference < compGuessDifference) {
+    return true;
+}else if (humanGuessDifference === compGuessDifference) {
+return true;
+} else {
+    return false;
+}
+}
+const updateScore = (winner) =>{
+    //let humanScore = 0;
+    //let computerScore = 0;
+
+    if (winner === 'human'){
+        humanScore +=1;
+    } else if (winner === 'computer'){
+        computerScore +=1;
+    }
+}
+const advanceRound = () =>{
+    currentRoundNumber+=1;
 }
